9a7d716be911203f1c11a8516a68d6cadccf1b7a
Christian Cardozo <chriiscardozo@gmail.com>
2017-06-03 18:22:31 -0300

    updating project

---
+++ data_analisys.py
@@ -0,0 +1,50 @@
+import numpy as np
+import json
+
+DATA_PATH='/home/christian/Downloads/steam_users_json.txt'
+CS_ID='730'
+
+def main():
+    f = open(DATA_PATH, 'r')
+    friend_count = []
+    game_count = []
+    CSGO_friend_count = []
+    CSGO_game_count = []
+
+    for line in f:
+        u = json.loads(line)
+        friend_count.append(u['friend_count'])
+        game_count.append(u['game_count'])
+
+        if(CS_ID in u['games']):
+            CSGO_friend_count.append(u['friend_count'])
+            CSGO_game_count.append(u['game_count'])
+
+    print('*** General Stats ***')
+    print(str(len(friend_count)) + ' users')
+
+    print('Friends count avg: ' + str(np.mean(friend_count)))
+    print('Friends count std dev: ' + str(np.std(friend_count)))
+    print('Max friends count: ' + str(max(friend_count)))
+    print('Min friends count: ' + str(min(friend_count)))
+
+    print('Games count avg: ' + str(np.mean(game_count)))
+    print('Games count std dev: ' + str(np.std(game_count)))
+    print('Max game count: ' + str(max(game_count)))
+    print('Min game count: ' + str(min(game_count)))
+
+    print('*** CG:GO Stats ***')
+    print(str(len(CSGO_friend_count)) + ' users')
+
+    print('CS:GO Friends count avg: ' + str(np.mean(CSGO_friend_count)))
+    print('CS:GO Friends count std dev: ' + str(np.std(CSGO_friend_count)))
+    print('CS:GO Max friends count: ' + str(max(CSGO_friend_count)))
+    print('CS:GO Min friends count: ' + str(min(CSGO_friend_count)))
+
+    print('CS:GO Games count avg: ' + str(np.mean(CSGO_game_count)))
+    print('CS:GO Games count std dev: ' + str(np.std(CSGO_game_count)))
+    print('CS:GO Max game count: ' + str(max(CSGO_game_count)))
+    print('CS:GO Min game count: ' + str(min(CSGO_game_count)))
+
+if __name__ == '__main__':
+    main()
